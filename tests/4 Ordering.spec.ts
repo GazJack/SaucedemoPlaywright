5301c6178f25e009e1c6a1dbe11d5ab24408e2b6
GazJack <greta.jackaite@gmail.com>
2025-03-20 21:58:01 +0200

    4 Ordering Playwright

--- tests/4 Ordering.spec.ts
+++ tests/4 Ordering.spec.ts
@@ -1,4 +1,34 @@
 const { test, expect } = require('@playwright/test');
 const { login } = require('../fixtures/login');
 
+const users = [
+    'standard_user',
+    'problem_user',
+    'performance_glitch_user',
+    'error_user',
+    'visual_user'
+];
+
+test.describe('Test Case 4', () => {
+    users.forEach(user => {
+        test.describe(`Testing with user: ${user}`, () => {
+            test.beforeEach(async ({ page }) => {
+                await login(page, user, 'secret_sauce');
+            });
+
+            test('Purchasing', async ({ page }) => {
+                await page.click('[data-test="add-to-cart-sauce-labs-backpack"]');
+                await page.click('[data-test="shopping-cart-link"]');
+                await page.click('[data-test="checkout"]');
+                await page.fill('[data-test="firstName"]', 'Zebrė');
+                await page.fill('[data-test="lastName"]', 'Zebrauskė');
+                await page.fill('[data-test="postalCode"]', '92317');
+                await page.click('[data-test="continue"]');
+                await expect(page.locator('[data-test="checkout-summary-container"]')).toBeVisible();
+                await page.click('[data-test="finish"]');
+                await expect(page.locator('[data-test="checkout-complete-container"]')).toBeVisible();
+            });
+        });
+    });
+});
 
